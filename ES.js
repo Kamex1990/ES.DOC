a1fabdc97f63db4fb6b8986106ffc2524a3b3249
Kamex1990 <82541968+Kamex1990@users.noreply.github.com>
2021-05-29 16:15:04 -0400

    ES.DOC

---
+++ ES.js
@@ -0,0 +1,103 @@
+
+// ES7
+
+
+// .includes() method
+
+// "Hellooooo".includes(0);
+
+// const pets = ['cat', 'dog','bat'];
+
+
+// const square = (x) => x**2
+
+// const cube = (y) => y**3
+
+
+
+// ES8
+
+// .padStart()
+// .padEnd()
+
+// 'tutle'.padStart(10);
+// 'tutle'.padEnd(10);
+
+// const fun = (
+// 			a,
+// 			b,
+// 			c,
+// 			d,
+// 			e,
+// 			) => {
+// 			console.log(a);
+// 		}
+
+// fun (1,2,3,4,)
+
+// Object.keys
+
+
+// const obj = {
+
+// 	username0: 'Santa',
+// 	username1: 'Rudolf',
+// 	username2: 'John',
+// }
+
+// object.entries(obj).forEach(value => {
+// 	console.log(value);
+// })
+
+// object.keys(obj).forEach((key, index) => {
+// 	console.log(key, obj [key]);
+// })
+
+// object.values(obj).forEach(value => {
+// 	console.log(value);
+// })
+
+
+
+// obj.entries(obj).map(value => {
+// 	return value [1] + value [0].replace('username', "");
+// })
+
+
+// ES 10
+
+// .FLAT()
+
+// const array = [1, 2, 3, 4, 5];
+// array.flat();
+
+// const array2 = [1, 2, [3, 4], [5, 6]];
+// array2.flat();
+
+// const array3 = [, 2, [3, 4], [5], [6]];
+// array3.flat();
+
+// const entries = ['Bob', 'Sally', '', 'Cindy']
+// entries.flat()
+
+// flatMap()
+
+// const nationPark = ['lamb', 'scorpion', 'cow', 'lion', 'giraffe'];
+// const nationPark = nationPark.flatMap(creature => creature + '2');
+
+// const userEmail3 = '              eddy123@gmail.com';
+// const userEmail4 = 'eddy123@gmail.com           ';
+// console.log(userEmail1.trimStart());
+// console.log(userEmail2.trimEnd());
+
+
+// const namePro = [['Tom', 23], ['Derek', 32], ['Jack', 44], ['john', 45]];
+// namePro.entries(namePro);
+// const obj = object.fromEntries(userProfiles);
+// object.fromEntries(userProfiles);
+
+try{
+	4+6
+} catch{
+	
+}
